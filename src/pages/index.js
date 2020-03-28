9f849ce7aaa3557f8ca6fd9115a26a41d0e64d77
Hunter Caron <huntercaron@me.com>
2020-03-28 15:07:17 +0100

    Changing colorball

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ const IndexPage = () => (
         <ToPage to={"/link"} backgroundColor={"#79C150"} />
         <ToPage to={"/zdog"} backgroundColor={"#FFF6D6"} />
         <ToPage to={"/tilt"} backgroundColor={"#E8E0FF"} />
-        <ToPage to={"/line"} backgroundColor={"#000"} />
+        <ToPage to={"/line"} backgroundColor={"#ffffff"} />
       </motion.div>
     </motion.div>
   </>
